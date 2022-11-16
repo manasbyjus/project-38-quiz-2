1c3eb348c906b7c72be1d09318b4a71a94c3ebb0
manasbyjus <100676212+manasbyjus@users.noreply.github.com>
2022-11-16 21:23:47 +1100

    Add files via upload

---
+++ js/Contestant.js
@@ -0,0 +1,44 @@
+class Contestant {
+  constructor(){
+    this.index = null;
+    this.answer = 0;
+    this.name = null;
+  }
+
+  getCount(){
+    var contestantCountRef = database.ref('contestantCount');
+
+     contestantCountRef.on("value",(data)=>{
+       contestantCount = data.val();
+     })
+
+
+    // contestantCountRef.on("value",()=>{
+    //   contestantCount = data.val();
+    // })
+
+    // contestantCountRef.on("value",(data)=>{
+    //   contestantCount = data();
+    // })
+
+  }
+
+  updateCount(count){
+ 
+
+
+    // database.ref('/').update({
+    //   contestantCount: 0
+    // });
+
+
+
+
+     database.ref('/').update({
+       contestantCount: count
+     });
+
+  }
+
+ 
+}
